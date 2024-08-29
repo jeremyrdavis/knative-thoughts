2024cfd3bab22474787b1e6292b2a1087c776cf9
Jeremy Davis <jeremyrdavis@mac.com>
2024-08-28 22:01:21 -0400

    qute working

--- knative-thoughts-frontend/src/main/java/com/redhat/developerscorner/knative/thoughts/frontend/FrontendApi.java
+++ knative-thoughts-frontend/src/main/java/com/redhat/developerscorner/knative/thoughts/frontend/FrontendApi.java
@@ -1,23 +1,30 @@
 package com.redhat.developerscorner.knative.thoughts.frontend;
 
 import io.quarkus.logging.Log;
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
-@Path("/thoughts")
+@Path("/")
 public class FrontendApi {
 
     @RestClient
     ThoughtRESTClient thoughtRESTClient;
 
+    @Inject
+    Template thought;
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String thoughtOfTheDay() {
-        Thought thought = thoughtRESTClient.thoughtOfTheDay();
-        Log.debugf("Thought of the day: %s", thought);
-        return "%s\n- %s".formatted(thought.text(), thought.author());
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance thoughtOfTheDay() {
+        Thought thoughtOfTheDay = thoughtRESTClient.thoughtOfTheDay();
+        Log.debugf("Thought of the day: %s", thoughtOfTheDay);
+        return thought.data("author", thoughtOfTheDay.author()).data("text", thoughtOfTheDay.text());
+//        return "%s\n- %s".formatted(thought.text(), thought.author());
     }
 }
